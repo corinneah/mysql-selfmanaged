7a75147b2911a5f808fff1e2bbeb13330016b3ca
corinneah <103472876+corinneah@users.noreply.github.com>
2022-10-26 01:26:18 -0400

    Update assig5.py

--- assig5.py
+++ assig5.py
@@ -1,1 +1,22 @@
+# import packages 
+import pandas as pd 
+import os 
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+
+# load variables 
+load_dotenv()
+
+#login/user information 
+MYSQL_HOSTNAME = os.getenv('MYSQL_HOSTNAME')
+  
+MYSQL_USER = os.getenv('MYSQL_USERNAME') 
+  
+MYSQL_PASSWORD = os.getenv('_Passwordmysql') 
+  
+MYSQL_DATABASE = os.getenv('MYSQL_DATABASE')
+
+# connecitng to a database(db)
+connection_string = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOSTNAME}/{MYSQL_DATABASE}'
+db = create_engine(connection_string)
 
