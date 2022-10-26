2375b2f48c19f073b2e07d5fe9c52c02a4d8445c
corinneah <103472876+corinneah@users.noreply.github.com>
2022-10-26 02:39:36 -0400

    Update assig5.py

--- assig5.py
+++ assig5.py
@@ -19,4 +19,14 @@ MYSQL_DATABASE = os.getenv('MYSQL_DATABASE')
 # connecitng to a database(db)
 connection_string = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOSTNAME}/{MYSQL_DATABASE}'
 db = create_engine(connection_string)
+print (db.table_names()) 
 
+query = 'SELECT * FROM sqltable.patients;' 
+
+patients_df = pd.read_sql(query, con=db) 
+patients_df 
+  
+sql_df = pd.read_csv('data/HHS_Unaccompanied_Children_Program.csv') 
+sql_df 
+  
+sql_df.to_sql('patients', con=db, if_exists='replace') 
